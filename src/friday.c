541612d23ead582d1e0668b0247716295b15096c
Andres Lopez <lopeza511@gmail.com>
2015-12-11 09:57:11 -0800

    added time modified to response headers

--- src/friday.c
+++ src/friday.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "assets.h"
 
@@ -10,23 +12,42 @@
 
 int		serve_index(struct http_request *);
 int		serve_css(struct http_request *);
-char*	read_file(char [], long *);
+char*	read_file(char [], long *, time_t *);
 
 int
 serve_css(struct http_request *req)
 {
-	// Read requested file, if not found return 404.
-	char *static_path = getenv("static_path");
-	char filename[255];
-	sprintf(filename, "%s%s", static_path, req->path);
-	long asset_len_css;
-	char *asset_css = read_file(filename, &asset_len_css);
+	char				*static_path = getenv("static_path");
+	char				filename[255];
+	long				asset_len_css;
+	time_t				asset_mtime_css;
 
+	// Read requested file, if not found return 404.
+	sprintf(filename, "%s%s", static_path, req->path);
+	char *asset_css = read_file(filename, &asset_len_css, &asset_mtime_css);
+	
 	if (asset_css == NULL) {
 		http_response(req, 404, NULL, 0);
 	} else {
-		http_response_header(req, "content-type", "text/css");
-		http_response(req, 200, asset_css, asset_len_css);
+		char		*date;
+		time_t		tstamp = 0;
+		
+		if (http_request_header(req, "if-modified-since", &date)) {
+			tstamp = kore_date_to_time(date);
+		}
+
+		// Make sure not to return if the modified since header was received.
+		if (tstamp != 0 && tstamp <= asset_mtime_css) {
+			http_response(req, 304, NULL, 0);
+		} else {
+			date = kore_time_to_date(asset_mtime_css);
+			if (date != NULL)
+				http_response_header(req, "last-modified", date);
+
+			// Serve CSS.
+			http_response_header(req, "content-type", "text/css");
+			http_response(req, 200, asset_css, asset_len_css);
+		}
 	}
 
 	return (KORE_RESULT_OK);
@@ -85,11 +106,12 @@ serve_index(struct http_request *req)
 }
 
 char*
-read_file(char filename[255], long *filesize)
+read_file(char filename[255], long *filesize, time_t *lmd)
 {
 	FILE	*fp = fopen(filename, "r");
 	char	*contents = NULL;
 	long	size;
+	struct stat attr;
 
 	if (fp == NULL) {
 		printf("File `%s` not found.\n", filename);
@@ -104,7 +126,11 @@ read_file(char filename[255], long *filesize)
 	
 	fclose(fp);
 
+	stat(filename, &attr);
+	
+	// Set stat pointers.
 	*filesize = size;
+	*lmd = attr.st_mtime;
 
 	return contents;
 }
